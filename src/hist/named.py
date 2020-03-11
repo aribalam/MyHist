6424dcbeb335ed4ba21730b6409044dd1fccef33
Arib Alam <aribalam64@gmail.com>
2020-03-12 01:41:02 +0530

    added indexing support with named axes

--- src/hist/named.py
+++ src/hist/named.py
@@ -6,27 +6,23 @@ class NamedHist(BaseHist):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Create a dict that maps the name to its corresponding axis
+        # Create a dict that maps the name to its corresponding axis number
         self.namedaxis = {}
 
-        for axis in self.axes:
+        for i, axis in enumerate(self.axes):
             if not axis.metadata.get('name', None):
                 raise KeyError('The name of axis required')
 
             # Create mapping from name to axis
-            self.namedaxis[axis.metadata['name']] = axis
-
-    # __init__.__doc__ = BaseHistogram.__init__.__doc__
+            self.namedaxis[axis.metadata['name']] = i
 
     def fill(self, *args, **kwargs):
         '''
-        Case 1: All values are passed as args
-                Just pass it to super.fill(...)
-        Case 2: All values are passed as kwargs
+        Case 1: All values are passed as kwargs
                 Get position of the axis from its name, pass it in that order
 
         checks:
-        If passed as kwargs, check if all axis values are passed
+            If passed as kwargs, check if all axis values are passed
         '''
 
         if len(args) > 0:
@@ -46,9 +42,32 @@ class NamedHist(BaseHist):
         # the final list is passed to the super class
         super().fill(*value_list, **kwargs)
 
-
-    #TODO: Override __getitem__ to allow named dict key access as well
     def __getitem__(self, item):
-        pass
+        '''
+        Case 1: Dict is provided
+                Axis name to number mapping. pass to super
+        '''
+
+        # the axis name could only be provided in the form of dict
+        # convert axis name to axis number in this case
+        if hasattr(item, "items"):
+            new_item = {}
+            for axis, val in item.items():
+
+                # if axis is provided as name and the name is a valid axis name
+                if isinstance(axis, str) and self.namedaxis.get(axis, None) is not None:
+                    new_item[self.namedaxis[axis]] = val
+                # if axis name is invalid
+                elif isinstance(axis, str):
+                    raise KeyError("Invalid axis name provided")
+
+                # axis is provided as number
+                else:
+                    new_item[axis] = val
+
+            return super().__getitem__(new_item)
+        else:
+            return super().__getitem__(item)
+
 
     #TODO: Implement a shortcut axis type bool - Integer axis with no underflow or overflow and two bins set to zero
