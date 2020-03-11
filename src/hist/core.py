4e70aa93c2f3fa8991b67cb1032a1b8d33557a27
Arib Alam <aribalam64@gmail.com>
2020-03-12 01:41:59 +0530

    added fix for certain indexing invloving bh.loc

--- src/hist/core.py
+++ src/hist/core.py
@@ -2,4 +2,15 @@ from boost_histogram import Histogram
 
 
 class BaseHist(Histogram):
-    pass
+
+    def _compute_commonindex(self, index):
+
+        # preprocessing for dict
+        if hasattr(index, "items"):
+            for i, val in index.items():
+
+                # check if callable (eg. bh.loc(...))
+                if callable(val):
+                    index[i] = val(self.axes[i])
+
+        return super()._compute_commonindex(index)
